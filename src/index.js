0ada997fa5c17f5541d78edd1d5d56404ce14dba
German Cabarcas <gcc@unironical.com>
2023-09-24 14:49:53 -0500

    Added handling for already shortened urls

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const chars =
   "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
 const generateCode = rndm.create(chars);
 
-const db = betterSqlite3('archive.sqlite');
+const db = betterSqlite3("archive.sqlite");
 db.prepare(
   "CREATE TABLE IF NOT EXISTS urls ( id INTEGER PRIMARY KEY, url TEXT NOT NULL, code TEXT NOT NULL, hits INTEGER NOT NULL)"
 ).run();
@@ -26,54 +26,46 @@ app.register(require("@fastify/view"), {
   },
 });
 
-app.register(require('@fastify/formbody'));
+app.register(require("@fastify/formbody"));
 
 app.get("/", (request, reply) => {
-  return reply.view('/src/home');
+  return reply.view("/src/home");
 });
 
 app.get("/archive", (request, reply) => {
   const archive = queryArchive.all();
-  return reply.view('src/archive', { archive });
+  return reply.view("src/archive", { archive });
 });
 
-app.post(
-  "/",
-    (request, reply) => {
-    if (
-      request.headers["content-type"] !==
-      "application/x-www-form-urlencoded"
-    ) {
-      reply.code = 400;
-      return "Bad Request incorrect content-type";
-    }
-    if (!/^https:\/\/(www\.)?bostonfirearms.com/i.test(request.body.url)) {
-      reply.code = 400;
-      return "Bad Request incorrect domain";
-    }
-
-    console.log('pre code generation');
-    const code = generateCode(6);
-    const codeHit = queryByCode.get(code);
-    const urlHit = queryByUrl.get(request.body.url);
-    console.log(codeHit, urlHit);
-    if (codeHit === undefined && urlHit === undefined) {
-      console.log('pre insert')
-      insert.run(request.body.url, code);
-      console.log('inserted')
-      reply.view('src/success', { code });
-    }
+app.post("/", (request, reply) => {
+  if (request.headers["content-type"] !== "application/x-www-form-urlencoded") {
+    reply.code = 400;
+    return "Bad Request incorrect content-type";
   }
-);
+  if (!/^https:\/\/(www\.)?bostonfirearms.com/i.test(request.body.url)) {
+    reply.code = 400;
+    return "Bad Request incorrect domain";
+  }
+
+  const code = generateCode(6);
+  const codeHit = queryByCode.get(code);
+  const urlHit = queryByUrl.get(request.body.url);
+  if (codeHit === undefined && urlHit === undefined) {
+    insert.run(request.body.url, code);
+    reply.view("src/success", { code });
+  }
+
+  if (urlHit) {
+    return reply.view("/src/repeaturl", { data: urlHit });
+  }
+});
 
 app.get("/:code", async (request, reply) => {
   const result = queryByCode.get(request.params.code);
   addHit.run(request.params.code);
-  return reply.view('/src/hit', { url: result.url })
+  return reply.view("/src/hit", { url: result.url });
 });
 
 app.listen({ port: 3000 });
 
-console.log(
-  `app running on http://localhost:3000`
-);
+console.log(`app running on http://localhost:3000`);
